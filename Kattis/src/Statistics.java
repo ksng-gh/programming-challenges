220795fe9a348a35921f7cfd0ca4216ec4fe9b97
sigge94 <sigge.94@live.se>
2020-01-27 00:28:57 +0100

    statistics

---
+++ Kattis/src/Statistics.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Statistics {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = 1;
+        int max = -10000001;
+        int min = 10000001;
+        while(sc.hasNext()){
+            int cases = sc.nextInt();
+            for(int i = 0; i < cases; i++){
+                int t = sc.nextInt();
+                if(max < t){
+                    max = t;
+                }
+                if(min > t){
+                    min = t;
+                }
+            }
+            System.out.println("Case " + n++ + ": " + min + " " + max + " " + (max - min));
+        }
+    }
+}
