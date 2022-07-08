b0c2f4fe1612a2bee2f591c6e92fc461ebe9075d
unknown <ksng@kth.se>
2022-07-08 19:41:59 +0200

    time limit

--- Kattis/Minibattleship.py
+++ Kattis/Minibattleship.py
@@ -1,5 +1,4 @@
 import copy
-from itertools import combinations
 
 n, k = map(int, input().split(" "))
 
@@ -7,21 +6,6 @@ battlefield = []
 ships = []
 
 def matchshipX(ships, field, start):
-
-    print("==========================")
-    print("start X")
-    print("==========================")
-
-    print("field start")
-    print(start)
-
-    print("curr field")
-    print(field)
-
-
-    print("ships")
-    print(ships)
-
 
 
     combinations = 0
@@ -33,10 +17,6 @@ def matchshipX(ships, field, start):
 
     for i in range(startx, startx + cship):
         coords.append((i, start[1]))
-
-    print("coords")
-    print(coords)
-
 
     for i in range(len(coords)):
         for j in range(len(field)):
@@ -54,42 +34,20 @@ def matchshipX(ships, field, start):
         if i[2] == "O":
             gatekeeper = True
 
-    print("field")
-    print(field)
-
-    if len(ships) == 0 and count == len(coords):
-        print("returns 1")
-        return 1
-    elif (len(ships) == 0 and len(field) != 0) or count != cship or (gatekeeper and len(ships) == 0):
-        print("returns 0")
+    if count != cship or (gatekeeper and len(ships) == 0):
         return 0
+
+    elif len(ships) == 0 and count == len(coords):
+        return 1
+
     else:
-        #deepcopyfield = copy.deepcopy(field)
-        #deepcopyships = copy.deepcopy(ships)
         for i in field:
-            #combinations += matchshipX(copy.deepcopy(ships), copy.deepcopy(field), i)# + matchshipY(copy.deepcopy(ships), copy.deepcopy(field), i)
             combinations += matchshipX(copy.deepcopy(ships), copy.deepcopy(field), i) + matchshipY(copy.deepcopy(ships), copy.deepcopy(field), i)
 
 
     return combinations
 
 def matchshipY(ships, field, start):
-
-    print("==========================")
-    print("start Y")
-    print("==========================")
-
-    print("field start")
-    print(start)
-
-    print("curr field")
-    print(field)
-
-
-    print("ships")
-    print(ships)
-
-
 
     combinations = 0
     cship = ships.pop(0)
@@ -105,9 +63,6 @@ def matchshipY(ships, field, start):
     for i in range(starty, starty + cship):
         coords.append((start[0], i))
 
-    print("coords")
-    print(coords)
-
     for i in range(len(coords)):
         for j in range(len(field)):
             if coords[i][0] == field[j][0] and coords[i][1] == field[j][1]:
@@ -124,21 +79,15 @@ def matchshipY(ships, field, start):
         if i[2] == "O":
             gatekeeper = True
 
-    print("field")
-    print(field)
-
-    if len(ships) == 0 and count == len(coords):
-        print("returns 1")
-        return 1
-    elif (len(ships) == 0 and len(field) != 0) or count != cship or gatekeeper:
-        print("returns 0")
+    if count != cship or (gatekeeper and len(ships) == 0):
         return 0
+
+    elif len(ships) == 0 and count == len(coords):
+        return 1
+
     else:
-        #deepcopyfield = copy.deepcopy(field)
-        #deepcopyships = copy.deepcopy(ships)
         for i in field:
             combinations += matchshipX(copy.deepcopy(ships), copy.deepcopy(field), i) + matchshipY(copy.deepcopy(ships), copy.deepcopy(field), i)
-            #combinations += matchshipY(copy.deepcopy(ships), copy.deepcopy(battlefield), i)
 
 
     return combinations
@@ -147,11 +96,6 @@ def calculate(ships, field):
     combinations = 0
 
     for i in field:
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        print("NEW")
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        #combinations += matchshipX(copy.deepcopy(ships), copy.deepcopy(battlefield), i)# + matchshipY(copy.deepcopy(ships), copy.deepcopy(battlefield), i)
-        #combinations += matchshipY(copy.deepcopy(ships), copy.deepcopy(battlefield), i)
         combinations += matchshipX(copy.deepcopy(ships), copy.deepcopy(battlefield), i) + matchshipY(copy.deepcopy(ships), copy.deepcopy(battlefield), i)
 
     return combinations
